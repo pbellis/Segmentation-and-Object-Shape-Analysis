51e940a26353e19084eb5506058966aadb84878e
Arjun Lamba <arjunlam@bu.edu>
2016-10-04 17:57:00 -0400

    update

--- object_analysis.h
+++ object_analysis.h
@@ -12,3 +12,9 @@ void calculate_centroid(cv::Mat& src, ushort lable, cv::Rect2i &bounds, int &x, 
 
 void calculate_circularity(const int &a, const int &b, const int &c, const int &h, float &emin, float &emax, float &circularity);
 
+void ratioAreaToPerimeter(int& area, int& perimeter, int& ratio);
+
+void compactness(int& area, int& perimeter, float& compact);
+
+void preOrientation(const cv::Mat& src, const ushort& label, const cv::Rect2i& bounds, const cv::Point2i centroid, int& a, int& b, int& c);
+
